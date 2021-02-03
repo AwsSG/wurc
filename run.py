5af85b508e9289f31693386aba2752b3c0da7eb5
AwsSG <aws.sabah.gheni@gmail.com>
2021-02-03 23:59:05 +0000

    Added user authentication and profile template

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import (Flask, flash, render_template,
                    redirect, session, request, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -20,6 +21,77 @@ mongo = PyMongo(app)
 def getwurc():
     matches = mongo.db.job_list.find()
     return render_template("getwurc.html", job_matches=matches)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if the username already exist
+        existing_user = mongo.db.members.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.members.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/log_in", methods=["GET", "POST"])
+def log_in():
+    if request.method == "POST":
+        # check if the user exists
+        existing_user = mongo.db.members.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches what the user provided
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect username and/or password")
+                return redirect(url_for("log_in"))
+
+        else:
+            # username does not exist
+            flash("Incorrect username and/or password")
+            return redirect(url_for("log_in"))
+
+    return render_template("log_in.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session's user username from db
+    username = mongo.db.members.find_one(
+        {"username": session["user"]})["username"]
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("log_in"))
+
+
+@app.route("/log_out")
+def log_out():
+    # remove user from session cookie
+    flash("You have been logged out successfully!")
+    session.pop("user")
+    return redirect(url_for("log_in"))
 
 
 if __name__ == "__main__":
